8ba85b08f8341ab34845e6a363b05e095d462dfb
tobias.kaessmann@web.de <tobias.kaessmann@web.de>
2025-03-04 15:00:17 +0800

    added google test

--- Glove/test/test_main.cpp
+++ Glove/test/test_main.cpp
@@ -1,34 +1,10 @@
-#include <Arduino.h>
-#include <unity.h>
+#include <gtest/gtest.h>
+#include "foo.h" // Include your header files
 
-// Example function to test
-int add(int a, int b) {
-    return a + b;
+TEST(FooTest, HandlesPositiveInput) {
+    EXPECT_EQ(foo(1), 2);
 }
 
-void setUp() {
-  // This will run before each test
-}
-
-void tearDown() {
-  // This will run after each test
-}
-
-void test_add() {
-  TEST_ASSERT_EQUAL(3, add(1, 2));
-  TEST_ASSERT_EQUAL(5, add(2, 3));
-}
-
-void setup() {
-  // Start the Unity test framework
-  UNITY_BEGIN();
-  
-  // Run the test
-  RUN_TEST(test_add);
-  
-  UNITY_END();
-}
-
-void loop() {
-  // Nothing needed here
+TEST(FooTest, HandlesNegativeInput) {
+    EXPECT_EQ(foo(-1), 0);
 }
