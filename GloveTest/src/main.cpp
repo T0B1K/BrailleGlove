26bda683525f262a9ec27a77b3843658715bf773
tobias.kaessmann@web.de <tobias.kaessmann@web.de>
2025-03-04 16:15:47 +0800

    file fix

--- GloveTest/src/main.cpp
+++ GloveTest/src/main.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
 
 void setup() {
-    // Initialize serial communication at 115200 baud
     Serial.begin(115200);
-    // Wait for serial port to connect (needed for native USB)
     while (!Serial) {
         delay(10);
     }
