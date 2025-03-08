1ca94da7b4c27dfacd95658fe7651840545c593f
tobias.kaessmann@web.de <tobias.kaessmann@web.de>
2025-03-08 14:59:59 +0800

    refactoring of the chording scheme

--- GloveTest/src/Models/EncodingScheme/ChordingScheme.h
+++ GloveTest/src/Models/EncodingScheme/ChordingScheme.h
@@ -1,10 +1,16 @@
 #ifndef CHORDING_SCHEME_H
 #define CHORDING_SCHEME_H
 
-
-enum ChordingScheme {
-    OST_ENCODING,
-    SEQUENTIAL_ENCODING
+/**
+ * @enum ChordingScheme
+ * @brief Enum to define the encoding schemes for braille chord processing.
+ * 
+ * This enum is used to differentiate between two types of encoding schemes: 
+ * OST_ENCODING and SEQUENTIAL_ENCODING.
+ */
+enum ChordingScheme { 
+    OST_ENCODING,     ///< Encoding based on OST (One-Shot Timing) scheme.
+    SEQUENTIAL_ENCODING ///< Encoding based on sequential timing scheme.
 };
 
 #endif
--- GloveTest/src/Models/EncodingScheme/Encoding.h
+++ GloveTest/src/Models/EncodingScheme/Encoding.h
@@ -1,7 +1,6 @@
 #ifndef ENCODING_H
 #define ENCODING_H
 
-
 #ifdef UNIT_TEST
     #ifndef ARDUINO_MOCK_H
         #pragma once
@@ -11,23 +10,47 @@
     #include <Arduino.h>
 #endif
 
+/**
+ * @class Encoding
+ * @brief Base class for encoding operations.
+ * 
+ * This class provides the basic functions for encoding used by its child classes.
+ */
 class Encoding {
 public:
+    /**
+     * @brief Custom delay function to provide a non-blocking delay.
+     * 
+     * This function allows for a non-blocking delay (unlike the blocking `delay()` function in Arduino),
+     * so the program can continue execution while waiting.
+     * 
+     * @param timeInMs The delay duration in milliseconds.
+     */
     static void customDelay(unsigned long timeInMs) {
-        unsigned long startMillis = millis();  // Get the current time
+        unsigned long startMillis = millis();  ///< Get the current time
         while (millis() - startMillis < timeInMs) {
-            yield();  // the program doesn't stop
+            yield();  ///< The program continues executing other tasks
         }
     }
 
+    /**
+     * @brief Checks whether the given pin number is valid for the specified hand.
+     * 
+     * This function validates whether the pin number belongs to the correct hand (left or right)
+     * based on the actuator configuration.
+     * 
+     * @param number The pin number to be validated.
+     * @param hand The hand (left or right) for which the validation is being done.
+     * @return True if the pin is valid for the hand; false otherwise.
+     */
     static bool validIndex(int number, Hand hand){
         if ((hand == Left && number > SingeltonGloveSettings::getInstance().NUM_ACTUATORS) ||
             (hand == Right && number < SingeltonGloveSettings::getInstance().NUM_ACTUATORS + 1)) {
             return false;
-        }else{
+        } else {
             return true;
         }
-
     }
 };
+
 #endif
