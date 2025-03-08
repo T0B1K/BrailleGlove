77d062e7d83a7d4231cf54ebb1884e3fe4b42ddb
tobias.kaessmann@web.de <tobias.kaessmann@web.de>
2025-03-08 17:12:23 +0800

    documentation for the main.ino

--- GloveTest/src/main.ino
+++ GloveTest/src/main.ino
@@ -1,15 +1,28 @@
 #include "Controller/Controller.h"
 
-const bool IS_SLAVE = false; // Set to true for slave, false for master
-Controller controller(IS_SLAVE); //inits the controller
+// Define whether the device is a slave or a master.
+// Set to true for slave, false for master.
+const bool IS_SLAVE = false; ///< Flag indicating whether the device is a slave (true) or master (false).
+Controller controller(IS_SLAVE); ///< Initializes the controller with the appropriate role (master or slave).
 
+/**
+ * @brief Setup function for the device.
+ * 
+ * Initializes serial communication and sets up the controller.
+ * Only initializes serial if the code is not running in unit test mode.
+ */
 void setup() {
     #ifndef UNIT_TEST
-        Serial.begin(115200);  // Serial communication, if it's not the test class
+        Serial.begin(115200);  ///< Starts serial communication at 115200 baud if not running in unit test mode.
     #endif
-    controller.setup();
+    controller.setup(); ///< Calls the setup function of the controller.
 }
 
+/**
+ * @brief Main loop function for the device.
+ * 
+ * Continuously calls the loop function of the controller.
+ */
 void loop() {
-    controller.loop();
+    controller.loop(); ///< Calls the loop function of the controller.
 }
