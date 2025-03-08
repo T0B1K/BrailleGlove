32a8bd5b1a137fde45a843f5986c4207b84e3f76
tobias.kaessmann@web.de <tobias.kaessmann@web.de>
2025-03-08 15:02:57 +0800

    refactoring of the sequentiial encoding scheme

--- GloveTest/src/Models/EncodingScheme/SequentialEncoding.h
+++ GloveTest/src/Models/EncodingScheme/SequentialEncoding.h
@@ -6,16 +6,33 @@
 #include "../../Models/HandEnum.h"
 #include "Encoding.h"
 
+/**
+ * @class SequentialEncoding
+ * @brief Handles the sequential encoding scheme for actuators.
+ * 
+ * This class defines the sequential encoding scheme and how the actuators should be activated in sequence.
+ */
 class SequentialEncoding : public Encoding {
 public:
+    /**
+     * @brief Activates and deactivates an actuator based on the sequential encoding scheme.
+     * 
+     * This function handles the sequential encoding scheme by activating the actuator corresponding to the
+     * given pin number and hand (left or right). After activation, the actuator is deactivated after a 
+     * specified duration. A delay is applied both after activation and deactivation.
+     * 
+     * @param number The index number representing the actuator to be activated.
+     * @param actuators The array of actuator pointers to be used.
+     * @param hand The hand (left or right) to which the actuator belongs.
+     */
     static void handle(int number, Actuator** actuators, Hand hand) {
         if (validIndex(number, hand)) {
-            int actuatorIdx = (number - 1) % SingeltonGloveSettings::getInstance().NUM_ACTUATORS; // 3 actuators on each hand
+            int actuatorIdx = (number - 1) % SingeltonGloveSettings::getInstance().NUM_ACTUATORS;
             actuators[actuatorIdx]->activate();
             customDelay(SingeltonGloveSettings::getInstance().DURATION);
             actuators[actuatorIdx]->deactivate();
             customDelay(SingeltonGloveSettings::getInstance().SEQ_OFFSET);
-        }else{
+        } else {
             customDelay(SingeltonGloveSettings::getInstance().DURATION);
             customDelay(SingeltonGloveSettings::getInstance().SEQ_OFFSET);
         }
