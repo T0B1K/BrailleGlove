1eae40b37a99259db5575735b1369dbb343ad22b
tobias.kaessmann@web.de <tobias.kaessmann@web.de>
2025-03-08 14:24:34 +0800

    refactored vibration actuatror

--- GloveTest/src/ActuatorTypes/VibrationActuator.h
+++ GloveTest/src/ActuatorTypes/VibrationActuator.h
@@ -3,27 +3,51 @@
 
 #include "Actuator.h"
 
+/**
+ * @class VibrationActuator
+ * @brief A class representing a vibration actuator.
+ * 
+ * This actuator uses a digital output pin to control a vibration motor.
+ */
 class VibrationActuator : public Actuator {
   public:
+    /**
+     * @brief Constructor for VibrationActuator.
+     * 
+     * Initializes the vibration actuator by setting the specified pin as an output
+     * and turning off the vibration motor initially.
+     * 
+     * @param pin The GPIO pin to which the actuator is connected.
+     */
     VibrationActuator(int pin) : Actuator(pin, Vibration) {
-        pinMode(pin, OUTPUT);
-        digitalWrite(pin, LOW);
+        pinMode(pin, OUTPUT);      ///< Set the pin as an output.
+        digitalWrite(pin, LOW);    ///< Ensure the actuator is off initially.
         turnedOn = false;
     }
 
+    /**
+     * @brief Activates the vibration actuator.
+     * 
+     * Turns on the vibration motor if it is not already on.
+     */
     void activate() override {
-        if(!turnedOn){
+        if (!turnedOn) {
             turnedOn = true;
-            digitalWrite(pin, HIGH); // Turn on vibration
+            digitalWrite(pin, HIGH); ///< Turn on vibration.
         }
     }
 
+    /**
+     * @brief Deactivates the vibration actuator.
+     * 
+     * Turns off the vibration motor if it is currently on.
+     */
     void deactivate() override {
-        if(turnedOn){
+        if (turnedOn) {
             turnedOn = false;
-            digitalWrite(pin, LOW); // Turn off vibration
+            digitalWrite(pin, LOW); ///< Turn off vibration.
         }
     }
 };
 
-#endif
+#endif // VIBRATION_ACTUATOR_H
