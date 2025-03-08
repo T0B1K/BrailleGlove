45de84437771c50db004d6e32a41cd2a59053bcc
tobias.kaessmann@web.de <tobias.kaessmann@web.de>
2025-03-08 15:31:20 +0800

    refactor hand enum

--- GloveTest/src/Models/HandEnum.h
+++ GloveTest/src/Models/HandEnum.h
@@ -1,9 +1,15 @@
 #ifndef HAND_ENUM_H
 #define HAND_ENUM_H
 
+/**
+ * @enum Hand
+ * @brief Represents the hand (left or right) for the glove model.
+ * 
+ * This enum is used to specify whether the glove is intended for the left hand or the right hand.
+ */
 enum Hand {
-    Left,
-    Right
+    Left, ///< Represents the left hand.
+    Right ///< Represents the right hand.
 };
 
 #endif
